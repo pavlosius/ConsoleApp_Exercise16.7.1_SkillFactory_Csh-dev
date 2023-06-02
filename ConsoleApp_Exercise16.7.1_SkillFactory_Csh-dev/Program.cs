a434c94c8235cf9b5c3a89ddae08e813cf21e718
pavlosius <pavlosius@mail.ru>
2023-06-02 20:57:48 +0300

    Add project files.

---
+++ ConsoleApp_Exercise16.7.1_SkillFactory_Csh-dev/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.That(calculator.Miltiplication(300, 10) == 3000);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(300, 10) == 30);
+        }
+
+
+    }
+}
